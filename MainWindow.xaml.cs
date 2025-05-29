35037575e5452bdcf50dfc320d35266d073d07b2
Irina Suderevskaya <pm93.suderevskaya@gmail.com>
2025-05-29 18:53:44 +0700

    commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace WpfBaseApp
     {
 
         public ObservableCollection<Points> PointsCollection;
-        float cfx, cfy,ddd;
+        float cfx, cfy;
         public MainWindow()
         {
             InitializeComponent();
